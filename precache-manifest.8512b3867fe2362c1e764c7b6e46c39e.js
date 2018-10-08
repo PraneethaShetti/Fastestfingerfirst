3ffe78a0e95d363b34077f4911f4032bb8037a9a
PraneethaShetti <pshetti062@gmail.com>
2018-10-08 14:17:32 -0700

    Updates

---
+++ precache-manifest.8512b3867fe2362c1e764c7b6e46c39e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91d23dbe68a6ee237ab0",
+    "url": "/Fastestfingerfirst/static/js/runtime~main.91d23dbe.js"
+  },
+  {
+    "revision": "17341d8e9c53271a9d49",
+    "url": "/Fastestfingerfirst/static/js/main.17341d8e.chunk.js"
+  },
+  {
+    "revision": "b1004afee357a334fb64",
+    "url": "/Fastestfingerfirst/static/js/1.b1004afe.chunk.js"
+  },
+  {
+    "revision": "17341d8e9c53271a9d49",
+    "url": "/Fastestfingerfirst/static/css/main.d82f5c30.chunk.css"
+  },
+  {
+    "revision": "587d9efa9c7e3d7508365dab9b7d3023",
+    "url": "/Fastestfingerfirst/index.html"
+  }
+];
